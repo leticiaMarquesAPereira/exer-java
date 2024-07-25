e393ab0606e1e1f8b5e9c7028727cbc504f54fbf
leticiaMarquesAPereira <leticia.marques.a.p05@gmail.com>
2024-07-25 19:40:55 -0300

    Cálculo da área e comparação de dois triângulos (com POO)

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -3,6 +3,8 @@ package curso_programacao;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangulo;
+
 public class Main {
 	
 	public static void main (String[] args) {
@@ -10,25 +12,27 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double xA, xB, xC, yA, yB, yC;
+		Triangulo x, y;
+		x = new Triangulo();
+		y = new Triangulo();
 		
 		System.out.println("Medidas de X: ");
 		
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Medidas de Y: ");
 		
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC)/2;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c)/2;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 		
-		p = (yA + yB + yC)/2;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (y.a + y.b + y.c)/2;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 		
 		System.out.printf("Area do triângulo: %.4f%n", areaX);
 		System.out.printf("Area do triângulo: %.4f%n", areaY);
