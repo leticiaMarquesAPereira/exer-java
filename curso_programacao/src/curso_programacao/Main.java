8169de467ac32287e468983d352225ab61f2239e
leticiaMarquesAPereira <leticia.marques.a.p05@gmail.com>
2024-07-26 15:21:05 -0300

    Cálculo da área e comparação de dois triângulos (com módulo)

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,49 +1,43 @@
 package curso_programacao;
-
 import java.util.Locale;
-import java.util.Scanner;
-
+import java.util.Scanner; 
 import entities.Triangulo;
 
 public class Main {
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
+	
 		Triangulo x, y;
+	
 		x = new Triangulo();
 		y = new Triangulo();
-		
-		System.out.println("Medidas de X: ");
-		
+	
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Valores de X: ");
 		x.a = sc.nextDouble();
 		x.b = sc.nextDouble();
-		x.c = sc.nextDouble();
-		
-		System.out.println("Medidas de Y: ");
-		
+		x.c = sc.nextDouble();	
+	
+		System.out.println("Valores de Y: ");
 		y.a = sc.nextDouble();
 		y.b = sc.nextDouble();
-		y.c = sc.nextDouble();
-		
-		double p = (x.a + x.b + x.c)/2;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c)/2;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-		
-		System.out.printf("Area do triângulo: %.4f%n", areaX);
-		System.out.printf("Area do triângulo: %.4f%n", areaY);
-		
+		y.c = sc.nextDouble();	
+	
+		double areaX = x.area();
+		double areaY = y.area();
+	
+		System.out.println("X = " + areaX + ", e Y = " + areaY);
+	
 		if (areaX > areaY) {
-			
-			System.out.println("Larger area: X");
-		} else {
-			
-			System.out.println("Larger area: Y");
+			System.out.println("X é o maior.");
 		}
-		
+		else {
+			System.out.println("Y é o maior.");
+		}
+		sc.close();
 	}
+
 }
